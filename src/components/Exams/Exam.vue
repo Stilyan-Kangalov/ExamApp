e63e2967eacd7a5179abc7d5445c53d0f9cb0425
Stilyan Kangalov <stilyan.kangalov@gmail.com>
2020-02-03 03:33:46 +0200

    Automated commit by Speedy_Git on Mon 02/03/2020 03:33 AM

--- src/components/Exams/Exam.vue
+++ src/components/Exams/Exam.vue
@@ -1,10 +1,34 @@
 <template>
-    
+<q-item clickable v-ripple>
+    <q-item-section avatar top>
+        <q-avatar v-if="getMyDate < exam.startDate" icon="create" color="orange" text-color="white" />
+        <q-avatar v-else-if="getMyDate >= exam.startDate && getMyDate <= exam.endDate" icon="play_arrow" color="red" text-color="white" />
+        <q-avatar v-else icon="check" color="grey" text-color="white" />
+        </q-item-section>
+        <q-item-section>
+            <q-item-label lines="1">{{ exam.name }}</q-item-label>
+            <q-item-label caption>{{ exam.startDate }}</q-item-label>
+            </q-item-section>
+            <q-item-section side>
+        <q-icon v-if="getMyDate < exam.startDate" name="info" color="amber" />
+        <q-icon v-else-if="getMyDate >= exam.startDate && getMyDate <= exam.endDate" name="info" color="red" />
+        <q-icon v-else name="info" color="green" />
+        </q-item-section>
+        </q-item>
 </template>
 
 <script>
+import { date } from 'quasar'
+
 export default {
-    
+  props: ['exam', 'id', 'date'],
+  computed: {
+    getMyDate () {
+      let timeStamp = Date.now()
+      let myCurrentDate = date.formatDate(timeStamp, 'DD/MM/YYYY HH:mm')
+      return myCurrentDate
+    }
+  }
 }
 </script>
 
--- src/pages/ExamPage.vue
+++ src/pages/ExamPage.vue
@@ -3,22 +3,7 @@
     <q-list bordered padding class="rounded-borders" style="max-width: 80%">
       <q-item-label header><h1>Изпити</h1></q-item-label>
 
-      <q-item v-for="(exam, key) in exams" :key="key" clickable v-ripple>
-        <q-item-section avatar top>
-          <q-avatar v-if="getMyDate < exam.startDate" icon="create" color="orange" text-color="white" />
-          <q-avatar v-else-if="getMyDate >= exam.startDate && getMyDate <= exam.endDate" icon="play_arrow" color="red" text-color="white" />
-          <q-avatar v-else icon="check" color="grey" text-color="white" />
-        </q-item-section>
-        <q-item-section>
-          <q-item-label lines="1">{{ exam.name }}</q-item-label>
-          <q-item-label caption>{{ exam.startDate }}</q-item-label>
-      </q-item-section>
-      <q-item-section side>
-        <q-icon v-if="getMyDate < exam.startDate" name="info" color="amber" />
-        <q-icon v-else-if="getMyDate >= exam.startDate && getMyDate <= exam.endDate" name="info" color="red" />
-        <q-icon v-else name="info" color="green" />
-        </q-item-section>
-        </q-item>
+      <exam v-for="(exam, key) in exams" :key="key" :id="key" :exam="exam" :date="getMyDate" ></exam>
     </q-list>
   </div>
 </template>
@@ -40,6 +25,9 @@ export default {
       let myCurrentDate = date.formatDate(timeStamp, 'DD/MM/YYYY HH:mm')
       return myCurrentDate
     }
+  },
+  components: {
+    'exam': require('components/Exams/Exam.vue').default
   }
 }
 </script>
