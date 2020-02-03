9f4f54d14c665961aee869a300b5e30efb2480f5
Stilyan Kangalov <stilyan.kangalov@gmail.com>
2020-02-03 20:54:50 +0200

    Automated commit by Speedy_Git on Mon 02/03/2020 08:54 PM

--- src/pages/Admin.vue
+++ src/pages/Admin.vue
@@ -1,10 +1,68 @@
 <template>
-  <q-page padding><p>Контролен панел</p></q-page>
+  <div class="q-pa-md q-gutter-md">
+    <q-list bordered padding class="rounded-borders" style="max-width: 80%">
+      <q-item-label header><h1>Контролен панел</h1></q-item-label>
+      <q-item v-for="(exam, key) in exams" :key="key" clickable v-ripple>
+    <q-item-section avatar top>
+        <q-avatar v-if="getMyDate < exam.startDate" icon="create" color="orange" text-color="white" />
+        <q-avatar v-else-if="getMyDate >= exam.startDate && getMyDate <= exam.endDate" icon="play_arrow" color="red" text-color="white" />
+        <q-avatar v-else icon="check" color="grey" text-color="white" />
+        </q-item-section>
+        <q-item-section>
+            <q-item-label lines="1">{{ exam.name }}</q-item-label>
+            <q-item-label caption>{{ exam.startDate }}</q-item-label>
+            </q-item-section>
+            <q-item-section side>
+              <q-btn flat round color="primary" icon="lock_open" />
+            </q-item-section>
+            <q-item-section side>
+              <q-btn @click.stop="askToRemove(id)" flat round color="red" icon="highlight_off" />
+            </q-item-section>
+        </q-item>
+    </q-list>
+  </div>
 </template>
 
 <script>
-export default {}
+import { date } from 'quasar'
+import { mapGetters, mapActions } from 'vuex'
+
+export default {
+  data () {
+    return {
+      visible: true
+    }
+  },
+  props: ['exam', 'id'],
+  computed: {
+    ...mapGetters('exams', ['exams']),
+    getMyDate () {
+      let timeStamp = Date.now()
+      let myCurrentDate = date.formatDate(timeStamp, 'DD/MM/YYYY HH:mm')
+      return myCurrentDate
+    }
+  },
+  methods: {
+    ...mapActions('exams', ['askToRemove']),
+    askToRemove (id) {
+      this.$q.dialog({
+        title: 'Моля потвърдете:',
+        message: 'Желаете ли да изтриете изпита?',
+        cancel: true,
+        persistent: true
+      }).onOk(() => {
+        console.log('>>>> OK')
+      })
+    }
+  }
+}
 </script>
 
 <style scoped>
+h1 {
+    font-size: 1.5rem;
+    font-weight: 300;
+    line-height: 0rem;
+    letter-spacing: -0.01562em;
+}
 </style>
